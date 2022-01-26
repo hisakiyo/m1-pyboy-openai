f536e9f4d2c0c525dc04ce912e61a168d9cf2e80
hisakiyo <hisakiyo.games@gmail.com>
2022-01-26 14:09:03 +0100

    ia qlearning

--- ia_qlearrning.py
+++ ia_qlearrning.py
@@ -3,77 +3,45 @@ from pyboy import PyBoy, WindowEvent
 import gym
 import numpy as np
 
-scale = 1
-bootROM = None
 filename = 'mario.gb'
 pyboy = PyBoy(filename, game_wrapper=True)
+
 # PyBoyGymEnv
 mario = pyboy.game_wrapper()
 mario.start_game()
-pyboy.set_emulation_speed(1000)
+pyboy.set_emulation_speed(5)
 env = pyboy.openai_gym()
 
-# Q-table
-print(mario.fitness)
+Q = np.zeros([99999, 3])
 
-episodes = 30
-steps = 800
-fitness_old = 0
-
-actions = []
-best_actions = []
-
-for episode in range(episodes):
-    # Fitness (score) is in mario.fitness
+# Do QLearning
+for episode in range(2):
     print("Episode: ", episode)
-    # Reset fitness
     pyboy.send_input(WindowEvent.PRESS_ARROW_UP)
-    while mario.lives_left > 1:
-        action = env.action_space.sample()
+    while mario.lives_left > 0:
+        # Get State
+        state = mario.fitness
+        # Get Action
+        action = np.argmax(Q[state])
+        # Do Action
         if action == 0:
-            pyboy.send_input(WindowEvent.PRESS_BUTTON_A)
-        elif action == 1:
             pyboy.send_input(WindowEvent.PRESS_ARROW_RIGHT)
+            pyboy.tick()
+            pyboy.send_input(WindowEvent.RELEASE_ARROW_RIGHT)
+        elif action == 1:
+            pyboy.send_input(WindowEvent.PRESS_BUTTON_A)
         elif action == 2:
             pyboy.send_input(WindowEvent.RELEASE_BUTTON_A)
-        elif action == 3:
-            pyboy.send_input(WindowEvent.RELEASE_ARROW_RIGHT)
-        elif action == 4:
-            pyboy.send_input(WindowEvent.PRESS_ARROW_LEFT)
-        elif action == 5:
-            pyboy.send_input(WindowEvent.RELEASE_ARROW_LEFT)
-        # tick
-        pyboy.tick()
-        actions.append(action)
-    print("Fitness: ", mario.fitness)
-        
-    # Store actions if fitness is higher than before
-    if mario.fitness > fitness_old:
-        best_actions = actions
-        fitness_old = mario.fitness
-        print("New fitness: ", fitness_old)
+        # Get Reward
+        reward = mario.fitness - state
+        # Get Next State
+        next_state = mario.fitness
+        # Update Q
+        Q[state][action] = Q[state][action] + 0.1 * (reward + 0.9 * np.max(Q[next_state]) - Q[state][action])
+        # Print out current state
+        print("State: " + str(state) + " | Action: " + str(action) + " | Reward: " + str(reward) + " | Next State: " + str(next_state))
+    mario.reset_game()
 
-pyboy.set_emulation_speed(1)
-print(len(best_actions))
-# Play best actions
-mario.reset_game()
-pyboy.send_input(WindowEvent.PRESS_ARROW_UP)
-while mario.lives_left > 1:
-        # pick first element in best_actions
-        action = best_actions.pop(0)
-        if action == 0:
-            pyboy.send_input(WindowEvent.PRESS_BUTTON_A)
-        elif action == 1:
-            pyboy.send_input(WindowEvent.PRESS_ARROW_RIGHT)
-        elif action == 2:
-            pyboy.send_input(WindowEvent.RELEASE_BUTTON_A)
-        elif action == 3:
-            pyboy.send_input(WindowEvent.RELEASE_ARROW_RIGHT)
-        elif action == 4:
-            pyboy.send_input(WindowEvent.PRESS_ARROW_LEFT)
-        elif action == 5:
-            pyboy.send_input(WindowEvent.RELEASE_ARROW_LEFT)
-        # tick
-        pyboy.tick()
-print("Fitness: ", mario.fitness)
-    
+    print("Score: " + str(mario.fitness))
+np.savetxt("qtable.csv", Q, delimiter=",")
+        
